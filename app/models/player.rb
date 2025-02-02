416f12d5e125b6811120b51cdc58ba66888ecc3b
Vladimir Kidyaev <krios2146@gmail.com>
2025-02-02 16:50:39 +0700

    feat: add matches table and model & update models associations

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,4 @@
 class Player < ApplicationRecord
+  has_many :won_matches, class_name: "Match", foreign_key: "winner_id", dependent: :destroy
+  has_many :lost_matches, class_name: "Match", foreign_key: "winner_id", dependent: :destroy
 end
