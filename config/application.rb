5db57ba0e5560bc17f671325573bd0deca6c72c8
Vladimir Kidyaev <krios2146@gmail.com>
2025-02-04 01:01:45 +0700

    chore: add dtos parsing to rails

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module TennisScoreboard
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.autoload_paths << Rails.root.join("app/dtos")
   end
 end
